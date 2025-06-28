8337f888edfb57fb1eccea9e4cf899072b82770e
Tyron Landman <tyron.j.landman@gmail.com>
2025-06-29 11:02:23 +1200

    changed to typescript, installed tailwind and build initial state of boardGame surface

---
+++ chess-react/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import Board from "./components/Board.tsx";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <Board />;
+}
+
+export default App;
